a11a3d45bdedbcbc1af3db7792f31700a015a75e
Alex Tellez <alextellezyanes@gmail.com>
2025-05-04 18:56:58 +0200

    products and shopping bag - part 1

--- app/homepage/Categories.js
+++ app/homepage/Categories.js
@@ -1,14 +1,20 @@
 import styles from './categories.module.css';
 
-export default function Categories() {
+export default async function Categories() {
+    const data = await fetch('https://dummyjson.com/products/categories')
+    const categories = await data.json()
+
     return (
         <div className={styles.categories}>
-
             <h2>Our Categories</h2>
             <ul className={`${styles.wrapper} container`}>
-                <li className={styles.category}> Running Apparel </li>
-                <li className={styles.category}> Running Shoes </li>
-                <li className={styles.category}> Running Accessories </li>
+                {
+                    categories.map(category => (
+                        <li key={category.slug} className={styles.category}>
+                            {category.name}
+                        </li>
+                    ))
+                }
             </ul>
 
         </div>
